0e14546825f1b12e76e798f1f662a46607e58fa9
Ionut <barborionut15@gmail.com>
2024-06-28 14:13:09 +0300

    Modified cmp to work with a lot of snippits

--- lua/core/plugin_config/cmp_nvim.lua
+++ lua/core/plugin_config/cmp_nvim.lua
@@ -1,4 +1,6 @@
 local cmp = require("cmp")
+local luasnip = require("luasnip")
+
 require("luasnip.loaders.from_vscode").lazy_load()
 cmp.setup({
 	mapping = cmp.mapping.preset.insert({
@@ -6,13 +8,15 @@ cmp.setup({
 		["<C-f>"] = cmp.mapping.scroll_docs(4),
 		["<C-Space>"] = cmp.mapping.complete(),
 		["<C-e>"] = cmp.mapping.abort(),
+		["<CR>"] = cmp.mapping.confirm({ select = true }),
 		["<Tab>"] = cmp.mapping(function(fallback)
+			local col = vim.fn.col(".") - 1
 			if cmp.visible() then
-				cmp.select_next_item()
-			elseif luasnip.locally_jumpable(1) then
-				luasnip.jump(1)
-			else
+				cmp.select_next_item(select_opts)
+			elseif col == 0 or vim.fn.getline("."):sub(col, col):match("%s") then
 				fallback()
+			else
+				cmp.complete()
 			end
 		end, { "i", "s" }),
 		["<S-Tab>"] = cmp.mapping(function(fallback)
@@ -28,15 +32,27 @@ cmp.setup({
 		--cmp.mapping.confirm({ select = true }),
 	}),
 	sources = cmp.config.sources({
-		{ name = "nvim_lsp" },
-		{ name = "luasnip" }, -- For luasnip users.
+		{ name = "nvim_lsp", keyword_length = 1 },
+		{ name = "luasnip", keyword_length = 2 },
 	}, {
-		{ name = "buffer" },
+		{ name = "buffer", keyword_length = 3 },
 	}),
 	snippet = {
 		-- REQUIRED - you must specify a snippet engine
 		expand = function(args)
 			require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
 		end,
+	},
+	window = {
+		completions = cmp.config.window.bordered({
+			col_offset = -2,
+			side_padding = 0,
+			border = "double",
+			winhighlight = "Normal:NormalFloat,FloatBorder:FloatBorder,CursorLine:PmenuSel,Search:None",
+		}),
+		documentation = cmp.config.window.bordered({
+			border = "rounded",
+			winhighlight = "Normal:NormalFloat,FloatBorder:FloatBorder,CursorLine:PmenuSel,Search:None",
+		}),
 	},
 })
