ff52da9e1c15bbdfc5febca36f8a931ab2c9d90b
Ionut <barborionut15@gmail.com>
2024-07-28 19:17:48 +0300

    more rust setup

--- lua/core/settings.lua
+++ lua/core/settings.lua
@@ -2,44 +2,44 @@
 local command = vim.api.nvim_command
 
 --Cursor line
-command('set cursorline')
-command('set cursorcolumn')
-command('set noshowmode')
-command('set laststatus=0')
+command("set cursorline")
+command("set cursorcolumn")
+command("set noshowmode")
+command("set laststatus=0")
 
 --Tabs settings to work normally
-command('set tabstop=6')
-command('set shiftwidth=6')
-command('set softtabstop')
+command("set tabstop=6")
+command("set shiftwidth=6")
+command("set softtabstop")
 
---Number line 
-command('set relativenumber')
-command('set number')
+--Number line
+command("set relativenumber")
+command("set number")
 
 -- Enable sane clipboard usage
-command('set clipboard+=unnamedplus')
+command("set clipboard+=unnamedplus")
 vim.opt.listchars = {
-  eol = ' ',
-  space = ' ',
-  trail = ' ',
-  extends = '▶',
-  precedes = '◀',
+	eol = " ",
+	space = " ",
+	trail = " ",
+	extends = "▶",
+	precedes = "◀",
 }
 vim.opt.list = true
-		
+
 -- Show matches when seacrhing
-command('set showmatch')
-command('set nohlsearch')
+command("set showmatch")
+command("set nohlsearch")
 
 -- Make tabs visible
-vim.cmd [[	set list lcs=tab:\┃ 
+vim.cmd([[	set list lcs=tab:\┃ 
 		set encoding=utf-8
 		set list
 		set cursorline
 		set cursorcolumn
-]]
+]])
 
-vim.cmd [[
+vim.cmd([[
 map <F5> :call CompileRunGcc()<CR>
 func! CompileRunGcc() "weird code i found on github hope it fucking works
 exec "w"
@@ -52,8 +52,8 @@ exec "!time ./%<"
 elseif &filetype == 'java'
 exec "!javac %"
 exec "!time java -cp %:p:h %:t:r"
-elseif &filetype == 'rs'
-exec "!cargo r"
+elseif &filetype == 'rust'
+exec "!cargo run"
 elseif &filetype == 'sh'
 exec "!time bash %"
 elseif &filetype == 'lua'
@@ -70,4 +70,4 @@ exec "!~/.vim/markdown.pl % > %.html &"
 exec "!firefox %.html &"
 endif
 endfunc
-]]
+]])
